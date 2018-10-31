ad83335d8e37753fcd247d0a30c716b19a852f6e
Markus Manninen <markus.j.manninen@gmail.com>
2018-10-31 18:25:04 +0200

    Add welcome message and broadcast it to all users.

--- server/server.js
+++ server/server.js
@@ -15,13 +15,30 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     socket.emit('newMessage', {
-        from: 'John',
-        text: 'See you then',
-        createdAt: '123123'
+        from: 'Admin',
+        text: 'Welcome to the Rock!',
+        createdAt: new Date().getTime()
+    });
+    
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
     });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+        socket.broadcast.emit('newMessage', {
+            from: message.text,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 
     socket.on('disconnect', () => {
