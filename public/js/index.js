f1857ce8533d13f8f0e50424a416b0ef71330ade
Markus Manninen <markus.j.manninen@gmail.com>
2018-11-07 19:50:20 +0200

    Add styles to chat page

--- public/js/index.js
+++ public/js/index.js
@@ -18,11 +18,13 @@ socket.on('newMessage', function (data) {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+
+    var $messageTextbox = jQuery('[name=message]')
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: $messageTextbox.val()
     }, function () {
-
+        $messageTextbox.val('');
     });
 });
 
@@ -37,18 +39,22 @@ socket.on('newLocationMessage', function (data) {
     jQuery('#messages').append(li);
 });
 
-var locationButton = jQuery('#send-location');
-locationButton.on('click', function () {
+var $locationButton = jQuery('#send-location');
+$locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
 
+    $locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        $locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        $locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
